3c91564ae89da87cea6da830f9fab49adede2480
Dianajarenga <jarengadiana@gmail.com>
2021-07-23 16:17:43 +0300

    SchoolSystem

--- SchoolSystem/settings.py
+++ SchoolSystem/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'student',
     'trainer',
     'course'
-    'year_calendar',
+    
 ]
 
 MIDDLEWARE = [
--- SchoolSystem/urls.py
+++ SchoolSystem/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from . import views
+from django.urls.conf import include
+
 
 app_name = 'year_calender'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^index/$', views.index, name='index'),
+    path('student/',include("student.urls")),
+   
 ]
